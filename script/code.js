0a166c25c89b0017d43e83cfb567925c97bb3086
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-01 02:00:15 +0200

    thelist

--- script/code.js
+++ script/code.js
@@ -1,8 +1,20 @@
-let input = document.querySelector('[data-input]')
 let btnDel = document.querySelector('[data-delete]')
-
+let groceries = document.querySelector('[data-list]')
+let list = []
 
 function list(){
-    let list = []
+    let input = document.querySelector('[data-input]').value
     
+    list.push(input)
+    localStorage.setItem('listItems', JSON.stringify(list))
+    
+}
+document.querySelector('[data-add]').
+addEventListener('click', list)
+
+function remove(){
+    list.pop(list)
+
+    document.write(groceries)
+
 }
